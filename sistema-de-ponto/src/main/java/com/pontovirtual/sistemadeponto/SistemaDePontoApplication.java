f0b42b61f2448aca9aaf37b5fae94a76327f3da4
Vini <marcosviniciosguedesdossantos@gmail.com>
2024-10-08 16:57:39 -0300

    start projeto spring.

---
+++ sistema-de-ponto/src/main/java/com/pontovirtual/sistemadeponto/SistemaDePontoApplication.java
@@ -0,0 +1,13 @@
+package com.pontovirtual.sistemadeponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePontoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDePontoApplication.class, args);
+    }
+
+}
---
+++ sistema-de-ponto/src/test/java/com/pontovirtual/sistemadeponto/SistemaDePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontovirtual.sistemadeponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePontoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
