11a11840cf6d2f5260e41b37ed66d67f24d89922
LuisNettoo <santosneto.luis@gmail.com>
2024-10-10 17:23:24 -0300

    primeiro commit

---
+++ front-end/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>O marco tem que dá o ânus</Text>
+      <StatusBar style="auto" />
+      <Button title='Aceitar'>
+
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textDecorationColor: '#fff',
+  },
+});
